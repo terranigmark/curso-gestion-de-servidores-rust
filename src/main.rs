0f6dd634d00139e9f552c118366f6eb99996a291
Yuliana <yulireyji@gmail.com>
2020-07-03 08:38:32 -0500

    crear mensaje de bienvenida

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::io::prelude::*;
-use std::net::{TcpListener, TcpStream}; // Agregamos libreria TcpStream y TcpStream
+use std::net::{TcpListener, TcpStream};
+use std::fs; // Agregamos libreria para leer archivo
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7373").unwrap(); // Escuchar la dirección donde nos vamos a conectar
-
+    let listener = TcpListener::bind("127.0.0.1:7373").unwrap();
     for stream in listener.incoming() {
                 let stream = stream.unwrap();
 
@@ -17,5 +17,10 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    let contents = fs::read_to_string("index.html").unwrap(); 
+
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
