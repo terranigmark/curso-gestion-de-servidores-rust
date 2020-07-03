ce6fa49a9408c01b24b5a04f382b23d060fa0c9c
Yuliana <yulireyji@gmail.com>
2020-07-03 08:28:28 -0500

    Mensaje de conectado en terminal

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,17 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7373").unwrap(); // Escuchar la dirección donde nos vamos a conectar
 
     for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                println!("¡Nuevo cliente!");
-            }
-                Err(e) => {
-                    println!("Conexión fallida")
-                }
-        }
-    }
+                let stream = stream.unwrap();
 
+                handle_connection(stream);
+            }
+
+
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
